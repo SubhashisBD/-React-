0bc7fecfd52701b091bb69b2b3a86d836d321772
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-01-09 23:25:06 +0530

    files added

--- Episode-04/App4.js
+++ Episode-04/App4.js
@@ -2,3 +2,52 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 console.log("BhanjDeo");
+
+/**
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Search Box
+ *  - Resturant Card
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ *
+ */
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://marketplace.canva.com/EAFaFUz4aKo/2/0/1600w/canva-yellow-abstract-cooking-fire-free-logo-JmYWTjUsE-Q.jpg"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      {/* Header */}
+      <Header />
+      // Body //Footer
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
