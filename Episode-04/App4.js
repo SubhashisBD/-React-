cf99362eb21ca981b7a720d654eade8c51680d9a
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-01-11 00:06:28 +0530

    changes

--- Episode-04/App4.js
+++ Episode-04/App4.js
@@ -42,10 +42,19 @@ const Header = () => {
   );
 };
 
+// Basically style takes js Object.
+const styleCard = {
+  backgroundColor : "#f0f0f0"
+};
+
 const Resturantcard = () => {
   return (
-    <div className="res-card">
+    <div className="res-card" style={styleCard}>
+      <img className="res-logo" src="https://b.zmtcdn.com/data/pictures/4/18766564/e74b217c7e258776494a73ca362e585e.jpeg?output-format=webp&fit=around|771.75:416.25&crop=771.75:416.25;*,*" alt="logo"></img>
       <h3>Meghna Foods</h3>
+      <h4>North Indian,Asian</h4>
+      <h4>4.4Star</h4>
+      <h4>35 minutes</h4>
     </div>
   );
 };
@@ -55,6 +64,11 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
+        <Resturantcard />
+        <Resturantcard />
+        <Resturantcard />
+        <Resturantcard />
+        <Resturantcard />
         <Resturantcard />
       </div>
     </div>
